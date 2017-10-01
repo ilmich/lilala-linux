3c8b410f335e1acc2c38d7763418f0830a34b95e
ilmich <ardutu@gmail.com>
2017-10-01 16:28:49 +0200

    Moved config files to new package named etc

---
+++ src/a/etc/doinst.sh
@@ -0,0 +1,23 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+config etc/HOSTNAME.new
+config etc/fstab.new
+config etc/group.new
+config etc/hosts.new
+config etc/init.d/rc.new
+config etc/inittab.new
+config etc/mdev.conf.new
+config etc/minirc.conf.new
+config etc/passwd.new
+config etc/profile.new
+config etc/syslog.conf.new
