23ad604be0532b3466707fbd43c4cdd0e42056a0
ilmich <ardutu@gmail.com>
2018-11-07 09:40:49 +0100

    [build]Lilala Linux v18.11

--- build.sh
+++ build.sh
@@ -2,30 +2,42 @@
 
 MAIN_DIR=$PWD
 
-if [ "$1" == "" ]; then
-    echo "Usage $0 platform_name"
+if [ ! -e $MAIN_DIR/output/platform ]; then
+    echo "Platform file not exists. Please run build_setup.sh first!!"
     exit 1
 fi
 
-if [ ! -e $PWD/platforms/$1/platform_env.sh ]; then
-    echo "Platform $1 not exists"
+PLATFORM_NAME=`cat $MAIN_DIR/output/platform`
+
+if [ ! -e $MAIN_DIR/platforms/$PLATFORM_NAME/platform_env.sh ]; then
+    echo "File platform_env.sh not found!! This is wrong!!!"
     exit 1
 fi
 
-. $PWD/platforms/$1/platform_env.sh
-PLATFORM_DIR=$PWD/platforms/$1
-OUTPUT_DIR=$MAIN_DIR/output/platforms/$1
+. $MAIN_DIR/platforms/$PLATFORM_NAME/platform_env.sh
+
+if [ -z "$PLATFORM_NAME" ]; then
+    echo "Unknown platform name. Please check platform setup"
+    exit 1
+fi
+
+PLATFORM_DIR=$PWD/platforms/$PLATFORM_NAME
+OUTPUT_DIR=$MAIN_DIR/output/platforms/$PLATFORM_NAME
 ROOTFS=$OUTPUT_DIR/rootfs
 STAGINGFS=$OUTPUT_DIR/staging
 KERNEL_DIR=$PLATFORM_DIR/kernel
 OUTPUT_PKGS=$OUTPUT_DIR/pkgs
+OUTPUT_LOGS=$OUTPUT_DIR/logs
 
-echo "Building lilala linux for"
+echo "Building Lilala linux for $PLATFORM_NAME"
+echo "----------------------------------------"
 echo "Target :$SLK_TARGET"
 echo "Toolchain path: $SLK_TOOLCHAIN_PATH"
 echo "Final rootfs: $ROOTFS"
 echo "Staging rootfs: $STAGINGFS"
 echo "Final pkgs output: $OUTPUT_PKGS"
+echo "----------------------------------------"
+echo "Check and press enter to start build"
 read
 
 export PATH=$PWD/tools/:$SLK_TOOLCHAIN_PATH:$PATH
@@ -36,37 +48,37 @@ mkdir -p $ROOTFS
 mkdir -p $STAGINGFS
 
 for i in \
-a/aaa_base \
-a/etc \
-a/tzdb \
-a/keymaps \
-l/musl \
-l/zlib \
-l/libmnl \
-l/libnftnl \
-l/libpcap \
-l/openssl \
-l/libnl \
-l/ncurses \
-a/busybox \
-a/e2fsprogs \
-a/kmod \
-a/dialog \
-n/openssh \
-n/iptables \
-n/wpa_supplicant \
-n/iw \
-n/hostapd \
-n/dnsmasq \
-n/wireless-tools;
+core/aaa_base \
+core/etc \
+core/tzdb \
+core/keymaps \
+core/musl \
+core/zlib \
+core/libmnl \
+core/libnftnl \
+core/libpcap \
+core/openssl \
+core/libnl \
+core/ncurses \
+core/busybox \
+core/e2fsprogs \
+core/kmod \
+core/dialog \
+core/openssh \
+core/iptables \
+core/wpa_supplicant \
+core/iw \
+core/hostapd \
+core/dnsmasq \
+core/wireless-tools;
 do
     PKG_DIR=$OUTPUT_PKGS/`dirname $i`
+    PKG_LOGS=$OUTPUT_LOGS/`dirname $i`
     PKG_NAME=`basename $i`
     PKGTYPE=tgz
     TAG=lilala
-    mkdir -p $PKG_DIR
-    if [ -e platforms/$1/src/$i ]; then
-        cd platforms/$1/src/$i
+    if [ -e platforms/$PLATFORM_NAME/src/$i ]; then
+        cd platforms/$PLATFORM_NAME/src/$i
     else
         cd src/$i
     fi
@@ -75,8 +87,10 @@ do
     PKGFINAL=$PKG_DIR/$PKG_NAME-$VERSION-$SLK_ARCH-$BUILD$TAG.$PKGTYPE
     if [ ! -e $PKGFINAL ]; then
         echo "Building $i"
+        mkdir -p $PKG_DIR
+	mkdir -p $PKG_LOGS
         SLK_TARGET=$SLK_TARGET SLK_SYSROOT=$STAGINGFS \
-        TAG=$TAG PKGTYPE=$PKGTYPE OUTPUT=$PKG_DIR STAGING=$STAGINGFS ./$PKG_NAME.SlackBuild &> $PKG_DIR/$PKG_NAME.log
+        TAG=$TAG PKGTYPE=$PKGTYPE OUTPUT=$PKG_DIR STAGING=$STAGINGFS ./$PKG_NAME.SlackBuild &> $PKG_LOGS/$PKG_NAME.log
 
         if [ $? -ne 0 ]; then
             echo "Error in $PKG_NAME.SlackBuild"
@@ -84,7 +98,7 @@ do
         fi
         if [ -e $PKGFINAL ]; then
             echo "Installing $i"
-            ROOT=$ROOTFS upgradepkg --reinstall --install-new $PKGFINAL &> $PKG_DIR/$PKG_NAME.install.log
+            ROOT=$ROOTFS upgradepkg --reinstall --install-new $PKGFINAL &> $PKG_LOGS/$PKG_NAME.install.log
         fi
     else
         echo "Skipping $i"
---
+++ build_setup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+MAIN_DIR=$PWD
+
+if [ "$1" == "" ]; then
+    echo "Usage $0 platform_name"
+    exit 1
+fi
+
+if [ ! -e $PWD/platforms/$1/platform_env.sh ]; then
+    echo "Platform $1 not exists"
+    exit 1
+fi
+
+. $PWD/platforms/$1/platform_env.sh
+OUTPUT_DIR=$MAIN_DIR/output
+mkdir -p $OUTPUT_DIR
+echo $1 > $OUTPUT_DIR/platform
+
+echo "Building lilala linux for"
+echo "Target :$SLK_TARGET"
