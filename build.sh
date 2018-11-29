81623a8628d6b5374340e2f3cc4c8f560bc1f72c
Michele Zuccala <ardutu@gmail.com>
2018-11-29 08:08:04 +0100

    [build]Refactoring main build script

--- build.sh
+++ build.sh
@@ -1,5 +1,42 @@
 #!/bin/sh
 
+function buildpkg() {
+
+    PKG_DIR=$OUTPUT_PKGS/`dirname $1`
+    PKG_LOGS=$OUTPUT_LOGS/`dirname $1`
+    PKG_NAME=`basename $1`
+    PKGTYPE=tgz
+    TAG=lilala
+    if [ -e platforms/$PLATFORM_NAME/src/$1 ]; then
+        cd platforms/$PLATFORM_NAME/src/$1
+    else
+        cd src/$1
+    fi
+    . ./$PKG_NAME.info
+    SLK_ARCH=`echo $SLK_TARGET | cut -d - -f 1 -`
+    PKGFINAL=$PKG_DIR/$PKG_NAME-$VERSION-$SLK_ARCH-$BUILD$TAG.$PKGTYPE
+    if [ ! -e $PKGFINAL ]; then
+        echo "Building $1"
+        mkdir -p $PKG_DIR
+#	mkdir -p $PKG_LOGS
+        SLK_TARGET=$SLK_TARGET SLK_SYSROOT=$STAGINGFS \
+        TAG=$TAG PKGTYPE=$PKGTYPE OUTPUT=$PKG_DIR STAGING=$STAGINGFS ./$PKG_NAME.SlackBuild # &> $PKG_LOGS/$PKG_NAME.log
+
+        if [ $? -ne 0 ]; then
+            echo "Error in $PKG_NAME.SlackBuild"
+            exit 1
+        fi
+        if [ -e $PKGFINAL ]; then
+            echo "Installing $i"
+            ROOT=$ROOTFS upgradepkg --reinstall --install-new $PKGFINAL #&> $PKG_LOGS/$PKG_NAME.install.log
+        fi
+    else
+        echo "Skipping $1"
+    fi
+    cd $MAIN_DIR
+
+}
+
 MAIN_DIR=$PWD
 
 if [ ! -e $MAIN_DIR/output/platform ]; then
@@ -20,6 +57,17 @@ if [ -z "$PLATFORM_NAME" ]; then
     echo "Unknown platform name. Please check platform setup"
     exit 1
 fi
+
+case $1 in
+    build)
+	echo "starting build"
+	shift
+	;;
+    *) 
+	echo "Command $1 not recognized"
+	exit 1
+	;;
+esac
 
 PLATFORM_DIR=$PWD/platforms/$PLATFORM_NAME
 OUTPUT_DIR=$MAIN_DIR/output/platforms/$PLATFORM_NAME
@@ -47,57 +95,14 @@ export PKG_CONFIG_LIBDIR=$STAGINGFS/usr/lib/pkgconfig:$STAGINGFS/usr/lib64/pkgco
 mkdir -p $ROOTFS
 mkdir -p $STAGINGFS
 
-for i in \
-core/aaa_base \
-core/etc \
-core/tzdb \
-core/keymaps \
-core/musl \
-core/zlib \
-core/sinit \
-core/ubase \
-core/sbase \
-core/mksh \
-core/kmod \
-core/libmnl \
-core/libnftnl \
-core/libpcap \
-core/openssl \
-core/e2fsprogs \
-core/openssh \
-core/iptables
-do
-
-    PKG_DIR=$OUTPUT_PKGS/`dirname $i`
-    PKG_LOGS=$OUTPUT_LOGS/`dirname $i`
-    PKG_NAME=`basename $i`
-    PKGTYPE=tgz
-    TAG=lilala
-    if [ -e platforms/$PLATFORM_NAME/src/$i ]; then
-        cd platforms/$PLATFORM_NAME/src/$i
-    else
-        cd src/$i
+while [ $# -gt 0 ] ; do
+    # scan for set of packages
+    BUILDLIST=`find src/ -name $1.buildlist`
+    COUNT=`echo $BUILDLIST | wc -l `
+    if [ $(($COUNT)) -eq 1 ]; then
+	for i in `cat $BUILDLIST`; do
+	    buildpkg $1/$i
+	done
     fi
-    . ./$PKG_NAME.info
-    SLK_ARCH=`echo $SLK_TARGET | cut -d - -f 1 -`
-    PKGFINAL=$PKG_DIR/$PKG_NAME-$VERSION-$SLK_ARCH-$BUILD$TAG.$PKGTYPE
-    if [ ! -e $PKGFINAL ]; then
-        echo "Building $i"
-        mkdir -p $PKG_DIR
-	mkdir -p $PKG_LOGS
-        SLK_TARGET=$SLK_TARGET SLK_SYSROOT=$STAGINGFS \
-        TAG=$TAG PKGTYPE=$PKGTYPE OUTPUT=$PKG_DIR STAGING=$STAGINGFS ./$PKG_NAME.SlackBuild # &> $PKG_LOGS/$PKG_NAME.log
-
-        if [ $? -ne 0 ]; then
-            echo "Error in $PKG_NAME.SlackBuild"
-            exit 1
-        fi
-        if [ -e $PKGFINAL ]; then
-            echo "Installing $i"
-            ROOT=$ROOTFS upgradepkg --reinstall --install-new $PKGFINAL #&> $PKG_LOGS/$PKG_NAME.install.log
-        fi
-    else
-        echo "Skipping $i"
-    fi
-    cd $MAIN_DIR
+    shift
 done
