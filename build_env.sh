bb0647d50040bd3c73c43741f02b8646df9302a3
Michele Zuccala <ardutu@gmail.com>
2019-10-13 08:42:58 +0000

    [pkg]fix rpi build

---
+++ build_env.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#export path in order to use tools
+#export PATH=$PWD/tools/:$PATH
+export LILALA_MAINDIR=$PWD
+alias lroot="cd $LILALA_MAINDIR"
--- platforms/rpi/platform_env.sh
+++ platforms/rpi/platform_env.sh
@@ -2,4 +2,4 @@ SLK_TOOLCHAIN_PATH=$MAIN_DIR/toolchains/armhf/
 SLK_TARGET=arm-linux-musleabihf
 SLK_ARCH=armv6j
 SLK_CFLAGS="-O2 -march=$SLK_ARCH -mtune=arm1176jzf-s -mfpu=vfp -mfloat-abi=hard"
-
+SLK_STRIP_PKG="yes"
