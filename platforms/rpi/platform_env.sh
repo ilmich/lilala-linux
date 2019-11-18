59eb80b617ac350eb5e58ac8aa1ecdc42d5c3ffe
ilmich <ardutu@gmail.com>
2019-11-18 11:28:16 +0100

    [pkg]remove useless .la files

--- platforms/rpi/platform_env.sh
+++ platforms/rpi/platform_env.sh
@@ -1,5 +1,5 @@
 SLK_TOOLCHAIN_PATH=$MAIN_DIR/toolchains/armhf/
 SLK_TARGET=arm-linux-musleabihf
 SLK_ARCH=armv6j
-SLK_CFLAGS="-O2 -march=$SLK_ARCH -mfpu=vfp -mfloat-abi=hard"
+SLK_CFLAGS="-O2 -march=$SLK_ARCH -mtune=arm1176jzf-s -mfpu=vfp -mfloat-abi=hard"
 SLK_STRIP_PKG="yes"
