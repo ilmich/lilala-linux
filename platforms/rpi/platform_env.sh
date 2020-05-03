4fb263c4df41c969322e6fab4a73206a2635ffc1
ilmich <ardutu@gmail.com>
2020-05-03 17:53:39 +0200

    [build]board should define cpu target

--- platforms/rpi/platform_env.sh
+++ platforms/rpi/platform_env.sh
@@ -4,16 +4,24 @@ SLK_STRIP_PKG="yes"
 
 case $SLK_BOARD in
     rpi)
-	SLK_ARCH=armv6j
-	SLK_CFLAGS="-O2 -march=$SLK_ARCH -mtune=arm1176jzf-s -mfpu=vfp -mfloat-abi=hard"
+	SLK_ARCH=armv6
+	SLK_CPU=arm1176jzf-s
+	SLK_CFLAGS="-O2 -mcpu=arm1176jzf-s -mfpu=vfp -mfloat-abi=hard"
 	;;
-    rpi2|rpi3)
+    rpi2)
 	SLK_ARCH=armv7a
-	SLK_CFLAGS="-O2 -march=armv7-a -mfpu=neon -mfloat-abi=hard"
+	SLK_CPU=cortex-a7
+	SLK_CFLAGS="-O2 -mcpu=cortex-a7 -mfpu=neon -mfloat-abi=hard"
+	;;
+    rpi3)
+	SLK_ARCH=armv8a
+	SLK_CPU=cortex-a53
+	SLK_CFLAGS="-O2 -mcpu=cortex-a53 -mfpu=neon -mfloat-abi=hard"
 	;;
     rpi4)
 	SLK_ARCH=armv8a
-	SLK_CFLAGS="-O2 -march=armv8-a -mtune=cortex-a72 -mfpu=neon -mfloat-abi=hard -ftree-vectorize -pipe -fomit-frame-pointer"
+	SLK_CPU=cortex-a72
+	SLK_CFLAGS="-O2 -mcpu=cortex-a72 -mfpu=neon -mfloat-abi=hard -ftree-vectorize -pipe -fomit-frame-pointer"
 	;;
     *)
 	echo "Unknown board!!!"
